6f94807906c7e3e64bd3ec3843632aed8ff87e51
NovikovValeriy <novikov.valera2005@gmail.com>
2025-03-02 21:02:07 +0300

    Initial commit

---
+++ Lab1/AnonymousBlock.sql
@@ -0,0 +1,10 @@
+DECLARE
+    v_id NUMBER := 1;
+BEGIN
+    FOR i IN 1..10000 LOOP
+        INSERT INTO MyTable (id, val) VALUES (v_id, ROUND(DBMS_RANDOM.VALUE(1, 1000)));
+        v_id := v_id + 1;
+    END LOOP;
+    COMMIT;
+END;
+/
---
+++ Lab1/EvenOddFunction.sql
@@ -0,0 +1,16 @@
+CREATE FUNCTION Check_Even_Odd RETURN VARCHAR2 IS
+    even_count NUMBER;
+    odd_count NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO even_count FROM MyTable WHERE MOD(val, 2) = 0;
+    SELECT COUNT(*) INTO odd_count FROM MyTable WHERE MOD(val, 2) <> 0;
+
+    IF even_count > odd_count THEN
+        RETURN 'TRUE';
+    ELSIF odd_count > even_count THEN
+        RETURN 'FALSE';
+    ELSE
+        RETURN 'EQUAL';
+    END IF;
+END;
+/
